e563fc209105ed0c6360c2d90229378f101eec8f
miya <rinn.sasami@gmail.com>
2021-02-02 11:11:39 +0900

    Ruby task Q20の修正

--- task.rb
+++ task.rb
@@ -227,24 +227,21 @@ class UserQ20
 end
 class Zoo
   # 以下に回答を記載
-  def initialize(name:, entry_fee:{})
+  def initialize(name:, entry_fee:)
     @name = name
-    @infant = entry_fee[:infant]    
-    @children = entry_fee[:children]
-    @adult = entry_fee[:adult]
-    @senior = entry_fee[:senior]
+    @entry_fee = entry_fee
   end
   
   def info_entry_fee(user)
     fee = case user.age 
     when 0..5
-      @infant
+      @entry_fee[:infant]
     when 6..12
-      @children
+      @entry_fee[:children]
     when 13..64
-      @adult
+      @entry_fee[:adult]
     when 65..120
-      @senior
+      @entry_fee[:senior]
     end
       puts "#{user.name}の入場料金は #{fee} 円です。"
   end
